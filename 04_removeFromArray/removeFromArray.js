58ca9fddc755d08be64834e63e3134b7bdc61611
Daniel Kenney <danielkenney99@yahoo.com>
2022-10-24 20:15:48 -0700

    problem 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removals) {
+    const output = array;
 
+    removals.forEach(removal => {
+        let index = array.indexOf(removal);
+        if (index >= 0) {
+            output.splice(index, 1)
+        }
+    })
+
+    return output;
 };
 
 // Do not edit below this line
