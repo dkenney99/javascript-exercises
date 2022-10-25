62dfe1f22f5960c7b1804c5f86d4b6a433d8be95
Daniel Kenney <danielkenney99@yahoo.com>
2022-10-24 21:10:46 -0700

    done with excercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let total = 0;
+    const allTheNumbers = [];
+
+    if (start > end) {
+        for (let i = end; i <= start; i++) {
+            allTheNumbers.push(i);
+        }
+    } else {
+        for (let i = start; i <= end; i++) {
+            allTheNumbers.push(i);
+        }
+    }
+
+    total = allTheNumbers.reduce(
+        (curr, acc) => curr + acc, 0
+    );
+
+    if (start < 0 || end < 0) {
+        return "ERROR"
+    } else if (typeof start == "string" || typeof end == "string") {
+        return "ERROR"
+    } else if (typeof start == "object" || typeof end == "object") {
+        return "ERROR"
+    }
+
+    return total;
 
 };
 
