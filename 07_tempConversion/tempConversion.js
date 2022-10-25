5ff3896029f4ae8018c998220de992636857128e
Daniel Kenney <danielkenney99@yahoo.com>
2022-10-24 21:37:56 -0700

    done with all excercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahr) {
+  const celsius = (fahr - 32) * 5/9;
+  return Math.round(celsius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cels) {
+  const fahrenheit = (cels * 9/5) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
