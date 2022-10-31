a63d89dd9f85c8290208900c0038118d410fbdbb
Daniel Kenney <danielkenney99@yahoo.com>
2022-10-30 20:45:04 -0700

    done calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const total = array.reduce((curr, next) => {
+    return curr + next;
+  }, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const total = array.reduce((curr, next) => {
+    return curr * next;
+  }, 1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(digit, exponent) {
+  let initial = 1;
+	for (let i = 0; i < exponent; i++) {
+    initial *= digit;
+  }
+  return initial;
 };
 
-const factorial = function() {
+const factorial = function fact(num) {
+  if (num == 0) {
+    return 1;
+  } else {
+    return fact(num - 1) * num;
+  }
 	
 };
 
