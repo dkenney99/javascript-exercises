0e744a90b9e9907a639058774d8f0a4ae5b5f79d
Daniel Kenney <danielkenney99@yahoo.com>
2022-10-31 11:26:33 -0700

    found the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = "";
+    let oldestAge = 0;
+    people.forEach(person => {
+        if (person.yearOfDeath == undefined) {
+            person.yearOfDeath = 2022;
+        }
+        if ((person.yearOfDeath - person.yearOfBirth) > oldestAge) {
+            oldestAge = (person.yearOfDeath - person.yearOfBirth);
+            oldest = person["name"]
+        }
+    });
+    console.log(oldest)
+    return oldest;
 };
 
 // Do not edit below this line
