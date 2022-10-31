ee27cc80d9b29075190677f7b129a652f9920836
Daniel Kenney <danielkenney99@yahoo.com>
2022-10-30 21:04:13 -0700

    plaindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    const noSpecial = phrase.replace(/[^a-zA-Z0-9 ]/g, '');
+    let noSpaceNoSpecial = noSpecial.replace(/ /g, "");
+    const finalString = noSpaceNoSpecial.toLowerCase();
+    const arrayOfString = finalString.split('');
+    let result = true;
+    let counterWeight = arrayOfString.length - 1;
+    console.log(arrayOfString)
+
+    for (let i = 0; i < arrayOfString.length; i++){
+        if (arrayOfString[i] != arrayOfString[counterWeight]) {
+            result = false;
+        }
+        counterWeight--
+    }
+
+    return result;
 
 };
 
