7c987f8aaebc744c263ac965101d0d0a870f4ccf
Daniel Kenney <danielkenney99@yahoo.com>
2022-10-31 10:22:22 -0700

    got the titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const arrayOfTitles = [];
+    books.forEach(book => {
+        arrayOfTitles.push(book["title"])
+    });
+    return arrayOfTitles;
 };
 
 // Do not edit below this line
